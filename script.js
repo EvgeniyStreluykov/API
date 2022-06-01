f9ff2ab14eca869088b17c6048df19f02d1bcc22
Evgeniy <streluykov@tut.by>
2022-06-01 13:27:19 +0300

    fixed fail

--- script.js
+++ script.js
@@ -42,13 +42,11 @@ loadImages = (data) => {
 
 window.onscroll = () => {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
+const scrollFunction = () => 
+  (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+  ? document.getElementById("myBtn").style.display = "block"
+  : document.getElementById("myBtn").style.display = "none";
+
 
 function topFunction() {
     document.body.scrollTop = 0;
