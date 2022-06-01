03f74643e445fbcee934eef2c0428dff70a789e5
Evgeniy <streluykov@tut.by>
2022-06-01 13:21:59 +0300

    fixed fail

--- script.js
+++ script.js
@@ -14,32 +14,34 @@ apiRequest = () => {
   const url = 'https://api.unsplash.com/search/photos?query='+input.value+'&per_page=30&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
 
   fetch(url)
-
   .then(response => {
     if (!response.ok) throw Error(response.statusText);
-      return response.json();
+      
+    return response.json();
    })
-
    .then(data => {
       loadImages(data);
    })
-
    .catch(error => console.log(error));
 }
 
 loadImages = (data) => {
   for(let i = 0; i < data.results.length; i++) {
-    let image = document.createElement("div");
+    const image = document.createElement("div");
+
     image.className = "img";
+    
     image.style.backgroundImage = "url("+data.results[i].urls.raw + "&w=1366&h=768" +")";
+    
     image.addEventListener("dblclick", function() {
       window.open(data.results[i].links.download, '_blank', rel = "noopener noreferrer");
     })
+    
     document.querySelector("#grid").appendChild(image);
   }
 }
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = () => {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -55,8 +57,8 @@ function topFunction() {
 }
 
 bgcolor = () => {
-  let date = new Date();
-  let hour = date.getHours();
+  const date = new Date();
+  const hour = date.getHours();
 
   if(hour >= 10 && hour < 20){
     document.body.style.backgroundColor = '#dee9fa';
